c6272c2e98c18dcf272d10171a7f3bf49b92d3d0
Marie Antons <maantons@mail.fhsu.edu>
2020-02-25 21:33:39 -0600

    added some logos and random lorem ipsum text as space filler

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@
 []Create an About Me guessing game that utilizes HTML, CSS, & JavaScript.
 [] Include on your HTML page a short biography, your education history, an overview of your job experience, and any goals that you may have.
 [] Prompt the user a total of exactly five yes or no questions. The user input for the answer must accept either y/n or yes/no responses while taking into consideration case sensitivity by normalizing the user input so that it can be validated (hint: look into the .toUpperCase() or .toLowerCase() functions). Be sure to let the user know if they answered the question correctly by alerting them with a response.
-[]Add console.log() messages within your app to notify the user if they are correct. Before submitting, comment out (don’t delete) your console.log() messages and replace them with the alerts() to complete the project.
-[] As a user, I would like a personalized welcome message so that I feel like I am interacting with the site owner.
-[] Ask the user their name through a prompt()
+[x]Add console.log() messages within your app to notify the user if they are correct. Before submitting, comment out (don’t delete) your console.log() messages and replace them with the alerts() to complete the project.
+[x] As a user, I would like a personalized welcome message so that I feel like I am interacting with the site owner.
+[x] Ask the user their name through a prompt()
 []Display that name back to the user through a custom greeting welcoming them to your site.
-[]Display the user’s name back to them in your final message to the user.
+[x]Display the user’s name back to them in your final message to the user.
 [x] STRETCH GOAL: Push yourself in your CSS work. Experiment, try some layout feature or technique that you have not tried before (maybe a sticky header or sticky footer, icon fonts such as FontAwesome, etc.) or just try to refine your layout/style as much as possible. We will not cover responsive design until Code 301, but if you have background in it, implement a breakpoint for a mobile view.
 */
 
@@ -17,29 +17,32 @@ console.log('This is working')
 
 
 
-var firstName = prompt(" Well hello there, I'm Marie, what's your name?");
-console.log('Our new best friends name:' + firstName);
-alert('Nice to meet you ' + firstName + ', ' + 'are you ready to become best friends?');
+// var firstName = prompt(" Well hello there, I'm Marie, what's your name?");
+// console.log('Our new best friends name:' + firstName);
+// alert('Nice to meet you ' + firstName + ', ' + 'are you ready to become best friends?');
 
-var lowerCaseName = firstName.toLowerCase();
+// var lowerCaseName = firstName.toLowerCase();
 
-if(lowerCaseName === 'marie'){
-    alert(firstName + ' ,' 'welcome back best friend!');
-  } else {
-    alert('Welcome Back!');
-  }
+// if(lowerCaseName === 'marie'){
+//     alert(firstName + ' ,' + 'welcome back best friend!');
+//   } else {
+//     alert('Welcome Back!');
+//   }
 
-var bermuda = prompt("Have I ever lived in the Carribean?");
-console.log('Have I ever lived in Carribean: ' + bermuda);
+// var bermuda = prompt("Have I ever lived in the Carribean?");
+// console.log('Have I ever lived in Carribean: ' + bermuda);
+// alert('Bermuda is not in the Carribean, its across from the Carolinas.')
 
-var ohio = prompt('Am I originally from Ohio?');
-console.log('State of residence:' + ohio);
+// var ohio = prompt('Am I originally from Ohio?');
+// console.log('State of residence:' + ohio);
+// alert('I am originally from Ohio')
 
-var husband = prompt('Am I married?');
-console.log('Married?' + husband);
+// var husband = prompt('Am I married?');
+// console.log('Married?' + husband);
+// alert('I am so married, woo!')
 
-var prevCareer = prompt('In my past career I was a Chef?');
-console.log('Previous Career answer:' + prevCareer);
- alert('You passed, ' + firstName + ', ' + 'we can now officially be best friends!');
+// var prevCareer = prompt('In my past career I was a Chef?');
+// console.log('Previous Career answer:' + prevCareer);
+//  alert('You passed, ' + firstName + ', ' + 'we can now officially be best friends!');
 
 
