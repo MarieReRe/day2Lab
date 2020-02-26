fcb165a070a1fffd5c2d36616ccc9a4a22990761
Marie Antons <maantons@mail.fhsu.edu>
2020-02-26 14:01:44 -0600

    changed header, background, deleted unused image files

--- js/app.js
+++ js/app.js
@@ -1,48 +1,60 @@
-/*
-[]As a user, I would like to learn about the site owner so I can get to know them better..
-[]Create an About Me guessing game that utilizes HTML, CSS, & JavaScript.
-[] Include on your HTML page a short biography, your education history, an overview of your job experience, and any goals that you may have.
-[] Prompt the user a total of exactly five yes or no questions. The user input for the answer must accept either y/n or yes/no responses while taking into consideration case sensitivity by normalizing the user input so that it can be validated (hint: look into the .toUpperCase() or .toLowerCase() functions). Be sure to let the user know if they answered the question correctly by alerting them with a response.
-[x]Add console.log() messages within your app to notify the user if they are correct. Before submitting, comment out (don’t delete) your console.log() messages and replace them with the alerts() to complete the project.
-[x] As a user, I would like a personalized welcome message so that I feel like I am interacting with the site owner.
-[x] Ask the user their name through a prompt()
-[]Display that name back to the user through a custom greeting welcoming them to your site.
-[x]Display the user’s name back to them in your final message to the user.
-[x] STRETCH GOAL: Push yourself in your CSS work. Experiment, try some layout feature or technique that you have not tried before (maybe a sticky header or sticky footer, icon fonts such as FontAwesome, etc.) or just try to refine your layout/style as much as possible. We will not cover responsive design until Code 301, but if you have background in it, implement a breakpoint for a mobile view.
-*/
 
-//do these two things first
-'use strict';
-console.log('This is working');
+//Assignment requirements
+//[] top 10 hobbies/restuarant as ordered list
+//[]work and education into ul
+//[]add feedback responses if missed question
+//[]Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
+//[]Indicates through an alert if the guess is “too high” or “too low”.
+//[] It should give the user exactly four opportunities to get the correct answer.
+//[] After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
+//[]As a user, I would like to guess the answer to a question that could have many possibilities so that I can have fun with with a guessing game.
+//[] Add a 7th question that has multiple possible correct answers that are stored in an array.
+//[] Give the user 6 attempts to guess the correct answer.
+//[]The guesses will end once the user guesses a correct answer or they run out of attempts.
+//[] Display all the possible correct answers to the user- use loop
+//[] Final score at end of quiz 
+//[] Use console.logs throughout the code to track the questions that are being asked to the user, and add addtional CSS to style your HTML page.
 
-
-
-var firstName = prompt(" Well hello there, I'm Marie, what's your name?");
-console.log('Our new best friends name:' + firstName);
-alert('Nice to meet you ' + firstName + ', ' + 'are you ready to become best friends?');
-
-var lowerCaseName = firstName.toLowerCase();
-
-if(lowerCaseName === 'marie'){
-    alert(firstName + ' ,' + 'welcome back best friend!');
-  } else {
-    alert('Welcome Back!');
-  }
-
-var bermuda = prompt("Have I ever lived in the Carribean?");
-console.log('Have I ever lived in Carribean: ' + bermuda);
-alert('Bermuda is not in the Carribean, its across from the Carolinas.');
-
-var ohio = prompt('Am I originally from Ohio?');
-console.log('State of residence:' + ohio);
-alert('I am originally from Ohio');
-
-var husband = prompt('Am I married?');
-console.log('Married?' + husband);
-alert('I am so married, woo!');
-
-var prevCareer = prompt('In my past career I was a Chef?');
-console.log('Previous Career answer:' + prevCareer);
- alert('You passed, ' + firstName + ', ' + 'we can now officially be best friends!');
+// Stretch Goals
+// []Attempt to make your code more DRY by putting all of the questions, answers, and responses to the first five yes/no questions into arrays (or even one huge multidimensional array), and modifying the game logic such that a ‘for’ loop will control the flow from question to question.
+//[]Make the number-guessing question use a random number as its correct answer.
 
 
+
+// window.addEventListener('load', (event) => {
+// //do these two things first
+// 'use strict';
+// console.log('This is working');
+
+
+
+// var firstName = prompt(" Well hello there, I'm Marie, what's your name?");
+// console.log('Our new best friends name:' + firstName);
+// alert('Nice to meet you ' + firstName + ', ' + 'are you ready to become best friends?');
+
+// var lowerCaseName = firstName.toLowerCase();
+
+// if(lowerCaseName === 'marie'){
+//     alert(firstName + ' ,' + 'welcome back best friend!');
+//   } else {
+//     alert('Welcome Back!');
+//   }
+
+// var bermuda = prompt("Have I ever lived in the Carribean?");
+// console.log('Have I ever lived in Carribean: ' + bermuda);
+// alert('Bermuda is not in the Carribean, its across from the Carolinas.');
+
+// var ohio = prompt('Am I originally from Ohio?');
+// console.log('State of residence:' + ohio);
+// alert('I am originally from Ohio');
+
+// var husband = prompt('Am I married?');
+// console.log('Married?' + husband);
+// alert('I am so married, woo!');
+
+// var prevCareer = prompt('In my past career I was a Chef?');
+// console.log('Previous Career answer:' + prevCareer);
+//  alert('You passed, ' + firstName + ', ' + 'we can now officially be best friends!');
+
+
+// });
