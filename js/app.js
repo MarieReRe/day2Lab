f7cb36d6f16e5cbf454c298efe6cedf8e13173e6
Marie Antons <maantons@mail.fhsu.edu>
2020-02-26 16:47:51 -0600

    added 6th question with prompts and randInt

--- js/app.js
+++ js/app.js
@@ -1,64 +1,84 @@
 
 //Assignment requirements
 //[x] top 10 hobbies/restuarant as ordered list
-//[]work and education into ul
+//[x]work and education into ul
 //[]add feedback responses if missed question
-//[]Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
-//[]Indicates through an alert if the guess is “too high” or “too low”.
-//[] It should give the user exactly four opportunities to get the correct answer.
-//[] After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
-//[]As a user, I would like to guess the answer to a question that could have many possibilities so that I can have fun with with a guessing game.
+//[x]Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
+//[x]Indicates through an alert if the guess is “too high” or “too low”.
+//[x] It should give the user exactly four opportunities to get the correct answer.
+//[x] After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
 //[] Add a 7th question that has multiple possible correct answers that are stored in an array.
 //[] Give the user 6 attempts to guess the correct answer.
 //[]The guesses will end once the user guesses a correct answer or they run out of attempts.
 //[] Display all the possible correct answers to the user- use loop
 //[] Final score at end of quiz 
-//[] Use console.logs throughout the code to track the questions that are being asked to the user, and add addtional CSS to style your HTML page.
+//[x] Use console.logs throughout the code to track the questions that are being asked to the user, and add addtional CSS to style your HTML page.
 
 // Stretch Goals
 // []Attempt to make your code more DRY by putting all of the questions, answers, and responses to the first five yes/no questions into arrays (or even one huge multidimensional array), and modifying the game logic such that a ‘for’ loop will control the flow from question to question.
-//[]Make the number-guessing question use a random number as its correct answer.
+//[x]Make the number-guessing question use a random number as its correct answer.
 
 
 
-// window.addEventListener('load', (event) => {
-// //do these two things first
-// 'use strict';
-// console.log('This is working');
+window.addEventListener('load', (event) => {
+  //do these two things first
+  'use strict';
+  console.log('This is working');
 
 
 
-// var firstName = prompt(" Well hello there, I'm Marie, what's your name?");
-// console.log('Our new best friends name:' + firstName);
-// alert('Nice to meet you ' + firstName + ', ' + 'are you ready to become best friends?');
+  // var firstName = prompt(" Well hello there, I'm Marie, what's your name?");
+  // console.log('Our new best friends name:' + firstName);
+  // alert('Nice to meet you ' + firstName + ', ' + 'are you ready to become best friends?');
 
-// var lowerCaseName = firstName.toLowerCase();
+  // var lowerCaseName = firstName.toLowerCase();
 
-// if(lowerCaseName === 'marie'){
-//     alert(firstName + ' ,' + 'welcome back best friend!');
-//   } else {
-//     alert('Welcome Back!');
-//   }
+  // if(lowerCaseName === 'marie'){
+  //     alert(firstName + ' ,' + 'welcome back best friend!');
+  //   } else {
+  //     alert('Welcome Back!');
+  //   }
 
-// var bermuda = prompt("Have I ever lived in the Carribean?");
-// console.log('Have I ever lived in Carribean: ' + bermuda);
-// alert('Bermuda is not in the Carribean, its across from the Carolinas.');
+  // var bermuda = prompt("Have I ever lived in the Carribean?");
+  // console.log('Have I ever lived in Carribean: ' + bermuda);
+  // alert('Bermuda is not in the Carribean, its across from the Carolinas.');
 
-// var ohio = prompt('Am I originally from Ohio?');
-// console.log('State of residence:' + ohio);
-// alert('I am originally from Ohio');
+  // var ohio = prompt('Am I originally from Ohio?');
+  // console.log('State of residence:' + ohio);
+  // alert('I am originally from Ohio');
 
-// var husband = prompt('Am I married?');
-// console.log('Married?' + husband);
-// alert('I am so married, woo!');
+  // var husband = prompt('Am I married?');
+  // console.log('Married?' + husband);
+  // alert('I am so married, woo!');
 
-// var prevCareer = prompt('In my past career I was a Chef?');
-// console.log('Previous Career answer:' + prevCareer);
-//  alert('You passed, ' + firstName + ', ' + 'we can now officially be best friends!');
-
-
+  // var prevCareer = prompt('In my past career I was a Chef?');
+  // console.log('Previous Career answer:' + prevCareer);
+  //  alert('You passed, ' + firstName + ', ' + 'we can now officially be best friends!');
 
 
+  function getRandomInt() {
+    var min = Math.ceil(0);
+    var max = Math.floor(10);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+  var answer = getRandomInt();
+  var randNum;
+  for (var i = 0; i < 4; i++) {
+    randNum = prompt('I am thinking of a number between 1-10, you have 4 guesses to get it right');
+    console.log(randNum);
+    if (answer == randNum) {
+      alert('Congrats you somehow guessed correctly!');
+      break;
+    } else if (answer > randNum) {
+      alert('Guess is too low');
+    } else if (answer < randNum) {
+      alert('Guess is too high, try again');
+    }
+  }
+  if (answer != randNum) {
+    alert('Sorry, you are out of attempts, the correct number is ' + answer);
+  }
 
 
-// });
+
+});
